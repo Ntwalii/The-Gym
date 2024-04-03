73c789507675a86c43a69a71c92ce4209f2c8fcc
Ntwalii <ntwaliaubin@gmail.com>
2024-04-03 17:50:43 +0200

    3rd April's learning journey

---
+++ sandbox.js
@@ -0,0 +1,60 @@
+// function toCamelCase(str){
+  
+//     let result=str.split('');
+      
+//       for (let i=0;i<=result.length;i++){
+       
+//         if(result[i]=="-"|| result[i]=="_"){
+       
+//           result[i+1]=result[i+1].toUpperCase();
+//           result.splice(i,1);
+//        }
+//       }
+//       return result.join('');
+//     }
+
+//     console.log(toCamelCase("the_stealth_warrior"));
+//     console.log(toCamelCase("The_Stealth_Warrior"));
+//     console.log(toCamelCase("The_Stealth-Warrior"));
+
+
+// // let result="camel";
+// // let arr= result.split('');
+// // console.log(arr);
+// // arr.splice(2,1);
+// // console.log(arr);
+
+
+
+// 2,4,5,6
+
+// write the function isAnagram
+// var isAnagram = function(test, original) {
+//     original=original.toLowerCase();
+//     test=test.toLowerCase();
+//     let first=0;
+//     let sec=0;
+    
+//     for(let i=0;i<original.length;i++){
+//       first+=original.charCodeAt(i);
+//     }
+//     for(let j=0;j<test.length;j++){
+//       sec+=test.charCodeAt(j);
+//     }
+//     return sec==first;
+//   };
+  
+
+//   console.log(isAnagram("Buckethead","DeathCubeK"));
+  
+
+
+
+
+
+
+// //Javascript history
+// //Strings
+// //Numbers
+// //Booleans
+// //
